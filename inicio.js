e7562ddfc6d90e6bbbf69e99d993dfa7c5785101
Simone Penido <simone_penido@hotmail.com>
2023-08-11 14:11:43 -0300

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,11 +1,18 @@
-var numero = parseInt(prompt('Digite um número:'));
+/* Solicitar ao usuário que digite um número inteiro, o programa irá verificar se o nnúmero atende aos critérios abaixo, sem exceção:
+Ser um número par;
+Ser um número positivo (maior que zero);
+Ser um número menor que 100.
+O programa irá imprimir a mensagem: "O número atende aos critérios", se atender a todos os critérios citados, caso contrário, deverá imprimir a mensagem: "O número não atende aos critérios".
+ */
+
+var numero = parseInt(prompt('Digite um número inteiro:'));
 
 
-if (numero % 2 == 0 && numero < 100){
+if (numero % 2 === 0 && numero > 0 && numero < 100){
    
-    alert('O número atende aos critérios');
+    alert("O número atende aos critérios.");
 
 
 } else {
-    alert('O número não atende aos critérios');
+    alert("O número não atende aos critérios.");
 }
